659b17c8673cc74882861211eec22c52dceb7f02
EverChicas <everchicasp@gmail.com>
2020-03-23 13:56:53 -0600

    pruebas de usurio

--- Salon/tests.py
+++ Salon/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
-
+from Salon.models import Salon
 # Create your tests here.
+class TestSalon(TestCase):
+    
+    def test_create_salon(self):
+        Salon(
+            Nombre="salon prueba",
+            Descripcion="una descripcion de salon",
+            Capacidad=100
+        ).save()
+        salones = Salon.objects.all()
+        salon = Salon.objects.get(id=1)
+        self.assertEquals(salones.count(),1)
+        self.assertEquals(salon.Nombre,"salon prueba")
