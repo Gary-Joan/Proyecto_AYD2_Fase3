c2cc197c846aa49d499619ca7190f4a17d050ad5
Gary Ortiz <goritz1490@gmail.com>
2020-03-25 07:31:12 -0600

    se agrego profiling y scripts de stress normal, pico, maxima

--- AYD2/settings.py
+++ AYD2/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'Montaje',
     'Restaurante',
     'Salon',
-    'User'
+    'User',
+    'silk'
 ]
 
 MIDDLEWARE = [
@@ -55,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'silk.middleware.SilkyMiddleware',
 ]
 
 ROOT_URLCONF = 'AYD2.urls'
